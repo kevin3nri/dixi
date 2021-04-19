0919fc8c99b3b8838b6cdedc46a9b6a8c8bdea91
Elizabeth <eli.oropeza.inf@gmail.com>
2021-04-19 01:54:44 -0500

    Nuevo CRUD y tabla profesor

---
+++ protected/controller/ControllerEditarprofesor.php
@@ -0,0 +1,19 @@
+<?php
+class controllerEditarprofesor extends Controller {
+    function __construct($view, $conf, $var, $acc) {
+        parent::__construct($view, $conf, $var, $acc);
+    } 
+    public function main() {
+        
+        foreach ($this->var as $key => $value) {
+            $this->data[$key]=$value;
+            $$key = $value;
+        }
+        $dominio = "profesor";
+        $this->data["datos"] = (object) indexModel::bd($this->conf)->getSQL("SELECT * FROM profesor WHERE id = {$idReg}")[0];
+        $this->data["accion"] = "Editar";
+        $this->data["dominio"] = "Profesor";
+        $this->view->show("editProfesor.twig", $this->data, $this->accion); 
+    }
+}
+?>
